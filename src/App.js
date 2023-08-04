84260defee217ea6ec5dc5b5fd7c8f5a52acd76e
AstroboyReloaded <astroboy.reloaded@gmail.com>
2023-08-04 12:57:38 -0600

    Install Dependencies

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import './App.css';
+import MainView from './components/MinView';
+
+function App() {
+  return (
+    <MainView />
+  );
+}
+
+export default App;
---
+++ src/components/MinView.js
@@ -0,0 +1,8 @@
+const MainView = () => (
+  <div className="main-view">
+    <p className="app-title">La Playita Management System</p>
+    <img className="main-image" src="./images/combi-surf.jpeg" alt="La Playita logo" />
+  </div>
+);
+
+export default MainView;
