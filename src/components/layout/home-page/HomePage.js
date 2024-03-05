cc7e739e5961be0feb5fc42c806dc1d3a8cc4a3c
AstroboyReloaded <astroboy.reloaded@gmail.com>
2024-03-05 04:16:45 -0600

    Create Punch btn

--- src/components/layout/home-page/HomePage.js
+++ src/components/layout/home-page/HomePage.js
@@ -1,9 +1,11 @@
 import Menu from './menu/Menu';
+import Punch from './punch/Punch';
 import homePage from './homePage.module.css';
 
 const HomePage = () => (
   <main className={homePage.mainContainer}>
     <Menu />
+    <Punch />
   </main>
 );
 
